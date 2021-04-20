1d5000204bd44bec4b202524306097552e84bb74
Jan Czok <jan.czok@outlook.de>
2021-04-20 13:30:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//place your code here
+// load instructions from the command input argument
+const instructions = process.argv[2] || "";
+// regex to allow only the letters 'G', 'L, and 'R'
+const regex = /^[GLR]*$/;
+
+if(instructions.match(regex)){
+    console.log(instructions);
+} else {
+    console.log("instructions are not valid")
+}
+
+// place your code here
